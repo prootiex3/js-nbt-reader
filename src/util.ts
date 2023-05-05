3331e359d26fcbcbf52a5913bb2679b8021b9e87
ConyPower <131973759+ConyPower@users.noreply.github.com>
2023-05-05 22:03:46 +0000

    Allow use of Uint8Array in util functions

--- src/util.ts
+++ src/util.ts
@@ -3,17 +3,19 @@ import pako from "pako";
 
 export type Optional<T> = T | undefined | null;
 
-export function is_gzip_compressed(bytes: Bytes) {
+export function is_gzip_compressed(bytes: Bytes | Uint8Array) {
 	try {
-		pako.ungzip(bytes.as_raw());
+		pako.ungzip(bytes instanceof Bytes ? bytes.as_raw() : bytes);
 		return true;
 	} catch (err) {
 		return false;
 	}
 }
 
-export function decompress(bytes: Bytes): Bytes {
+export function decompress(bytes: Bytes | Uint8Array) {
 	return is_gzip_compressed(bytes)
-		? Bytes.from(pako.ungzip(bytes.as_raw()))
+		? Bytes.from(
+				pako.ungzip(bytes instanceof Bytes ? bytes.as_raw() : bytes)
+		  )
 		: bytes;
 }
